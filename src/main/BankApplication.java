c2ae157e291ce887013b6a73d9bb9fa7d115df3a
Jitesh-17 <jiteshdas20@gmail.com>
2026-06-05 18:02:55 +0530

    Implemented deposit and withdraw methods

--- src/main/BankApplication.java
+++ src/main/BankApplication.java
@@ -7,11 +7,16 @@ public class BankApplication {
        AccountService service = new AccountService();
 
        Account acc = service.createAccount("Acc1001","jitesh",10000);
-
       System.out.println("Congratulations! Bank account created successfully");
       System.out.println("Account Number : " + acc.getAccountNumber());
       System.out.println("Name : " + acc.getHolderName());
       System.out.println("Balance : " + acc.getBalance());
+      
+      service.deposit(acc,5000);
+      System.out.println("\nAmount Deposit successful! \nCurrent Amount : " + acc.getBalance());
+
+        service.withdraw(acc,25000);
+        System.out.println("\nAmount withdrawal successful! \nCurrent Amount : " + acc.getBalance());
     }
 }
 
