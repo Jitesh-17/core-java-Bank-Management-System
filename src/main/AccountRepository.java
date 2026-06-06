c3366cb7a5817de7c04f167297d68b178c2968ab
Jitesh-17 <jiteshdas20@gmail.com>
2026-06-06 23:58:37 +0530

    created AccountRepository class with saveAccount,getAccount and getAllAccounts methods

---
+++ src/main/AccountRepository.java
@@ -0,0 +1,23 @@
+package main;
+import java.util.*;
+
+public class AccountRepository {
+    
+    private Map<String,Account> accounts = new HashMap<>();
+
+    public void saveAccount(Account account){
+        accounts.put (account.getAccountNumber(),account);
+    }
+
+    public Account findAccount(String accountNumber){
+        // if(!accounts.containsKey(accountNumber)){
+        //     System.out.println("invalid account number");
+        //     return ;
+        // }
+        return accounts.get(accountNumber);
+    }
+
+    public Collection<Account> getAllAccounts(){
+        return accounts.values();
+    }
+}
