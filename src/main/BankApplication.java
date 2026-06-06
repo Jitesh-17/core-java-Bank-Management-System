74e6610287be5c7e0fc51d68830e810b91587ec3
Jitesh-17 <jiteshdas20@gmail.com>
2026-06-06 23:54:33 +0530

    created Repository object

--- src/main/BankApplication.java
+++ src/main/BankApplication.java
@@ -2,21 +2,28 @@ package main;
 
 public class BankApplication {
     public static void main(String[] args){
-       System.out.println("Welcome to Bank Management System"); 
+        System.out.println("Welcome to Bank Management System"); 
 
-       AccountService service = new AccountService();
+        AccountRepository repository = new AccountRepository();
+        AccountService service = new AccountService(repository);
 
        Account acc = service.createAccount("Acc1001","jitesh",10000);
-      System.out.println("Congratulations! Bank account created successfully");
-      System.out.println("Account Number : " + acc.getAccountNumber());
-      System.out.println("Name : " + acc.getHolderName());
-      System.out.println("Balance : " + acc.getBalance());
+       System.out.println("Congratulations! Bank account created successfully");
+       System.out.println("Account Number : " + acc.getAccountNumber());
+       System.out.println("Name : " + acc.getHolderName());
+       System.out.println("Balance : " + acc.getBalance());
       
-      service.deposit(acc,5000);
-      System.out.println("\nAmount Deposit successful! \nCurrent Amount : " + acc.getBalance());
+       service.deposit(acc,5000);
+       System.out.println("\nAmount Deposit successful! \nCurrent Amount : " + acc.getBalance());
 
-        service.withdraw(acc,25000);
+        service.withdraw(acc,3000);
         System.out.println("\nAmount withdrawal successful! \nCurrent Amount : " + acc.getBalance());
+
+        Account foundAccount = service.getAccount("Acc1001");
+        System.out.println("account holder name:" + foundAccount.getHolderName());
+
+
+
     }
 }
 
