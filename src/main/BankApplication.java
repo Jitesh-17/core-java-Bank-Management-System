65b68bf0a897416b795e1a16798a47bed441ae4d
Jitesh-17 <jiteshdas20@gmail.com>
2026-06-10 16:55:20 +0530

    Menu driven Application

--- src/main/BankApplication.java
+++ src/main/BankApplication.java
@@ -5,22 +5,26 @@ public class BankApplication {
         System.out.println("Welcome to Bank Management System"); 
 
         AccountRepository repository = new AccountRepository();
+        
         AccountService service = new AccountService(repository);
 
-       Account acc = service.createAccount("Acc1001","jitesh",10000);
-       System.out.println("Congratulations! Bank account created successfully");
-       System.out.println("Account Number : " + acc.getAccountNumber());
-       System.out.println("Name : " + acc.getHolderName());
-       System.out.println("Balance : " + acc.getBalance());
+        BankMenu menu = new BankMenu(service);
+        menu.start();
+
+    //    Account acc = service.createAccount("Acc1001","jitesh",10000);
+    //    System.out.println("Congratulations! Bank account created successfully");
+    //    System.out.println("Account Number : " + acc.getAccountNumber());
+    //    System.out.println("Name : " + acc.getHolderName());
+    //    System.out.println("Balance : " + acc.getBalance());
       
-       service.deposit(acc,5000);
-       System.out.println("\nAmount Deposit successful! \nCurrent Amount : " + acc.getBalance());
+    //    service.deposit(acc,5000);
+    //    System.out.println("\nAmount Deposit successful! \nCurrent Amount : " + acc.getBalance());
 
-        service.withdraw(acc,3000);
-        System.out.println("\nAmount withdrawal successful! \nCurrent Amount : " + acc.getBalance());
+    //     service.withdraw(acc,3000);
+    //     System.out.println("\nAmount withdrawal successful! \nCurrent Amount : " + acc.getBalance());
 
-        Account foundAccount = service.getAccount("Acc1001");
-        System.out.println("account holder name:" + foundAccount.getHolderName());
+    //     Account foundAccount = service.getAccount("Acc1001");
+    //     System.out.println("account holder name:" + foundAccount.getHolderName());
 
 
 
