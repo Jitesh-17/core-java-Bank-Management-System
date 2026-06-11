e63abf05b3154f169dbdae991cf4189047ac688b
Jitesh-17 <jiteshdas20@gmail.com>
2026-06-11 23:29:22 +0530

    Created transaction object and added getTransaction method

--- src/main/Account.java
+++ src/main/Account.java
@@ -1,9 +1,15 @@
 package main;
-
+import java.util.*;
 public class Account{
     final private String accountNumber;
     final  private String holderName;
     private double balance;
+
+    public List<Transaction> transactions = new ArrayList<>();
+
+    public List<Transaction> getTransaction(){
+        return transactions;
+    }
 
     public Account(String accountNumber,String holderName,double balance){
         this.accountNumber = accountNumber;
